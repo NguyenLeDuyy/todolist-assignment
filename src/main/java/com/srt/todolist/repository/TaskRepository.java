09f8639cb806939ceebe6c8158a6aac39b2a4a1c
NguyenLeDuyy <nguyenleduy10122004@gmail.com>
2026-07-05 17:32:38 +0700

    feat: add TaskRepository with custom filter query

---
+++ src/main/java/com/srt/todolist/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.srt.todolist.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.srt.todolist.entity.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByCompleted(boolean completed);
+}
